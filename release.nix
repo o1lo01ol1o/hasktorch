a524211c0462cd42c292a544dda0d75c95cbadf2
o1lo01ol1o <tim.pierson@gmail.com>
2020-07-01 00:09:49 +0100

    build both systems

--- release.nix
+++ release.nix
@@ -1,1 +1,12 @@
-import ./default.nix {}
+{ supportedSystems ? [ "x86_64-linux" "x86_64-darwin" ]
+} : 
+let 
+currentNixpkgs = import <nixpkgs> {};
+in 
+ {
+  build = currentNixpkgs.lib.genAttrs supportedSystems (system:
+    import ./default.nix {inherit system; }
+    );
+ }
+
+  
